3fe0187945447613cd57eebc4cbb1a3a57696b53
JoseCarlos369 <cfj1010167@est.univalle.edu>
2023-09-17 14:37:20 -0400

    todo

---
+++ src/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    try {
+        mongoose.connect('mongodb+srv://kurmiproyect:iQE9cSYI7zzrxbgL@kurmiproyect.88vskcw.mongodb.net/kurmiproyect?retryWrites=true&w=majority');
+        console.log("DB connected");
+    } catch (error) {
+        console.log(error);
+        throw Error("DB Error");
+    }
+    };
